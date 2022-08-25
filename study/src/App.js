14421017b813179e97cd98b405ddca11d8fce348
erik9893 <73157098+erik9893@users.noreply.github.com>
2022-08-25 15:54:11 +0900

    React
    Final CodingApple - 6

--- study/src/App.js
+++ study/src/App.js
@@ -13,12 +13,22 @@ function App() {
     글제목변경(newArray);
   }
 
+  function 가나다순(){
+    var newArray = [...글제목];
+    newArray.sort();
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 Blog</div>
+        <div>React Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
+      
+      <button onClick={ 제목바꾸기 }>글바꾸기 정렬</button>
+
+      <button onClick={ 가나다순  }>가나다순정렬</button>
+
       <div className="list">
         <h4> { 글제목[0] } <span onClick={ ()=>{ 따봉변경( 따봉 + 1 ) } } >👍</span> { 따봉 }</h4>
         <p>8월 24일 발행</p>
@@ -36,8 +46,21 @@ function App() {
         <p>8월 22일 발행</p>
         <hr/>
       </div>
+
+      <Modal></Modal>
+
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
